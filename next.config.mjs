2f666253cc876f15ac677f8ff61bad770587d937
vijay <vijaykankala755@gmail.com>
2024-11-27 13:15:28 +0530

    bugfixes

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    env: {
+        // Load environment variables (ensure these are added securely)
+        MONGO_URI: process.env.MONGO_URI,
+    },
+};
 
 export default nextConfig;
