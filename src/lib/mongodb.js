ef30e3a4e986a79bb27817a2f565c47f7e49f00a
vijay <vijaykankala755@gmail.com>
2024-11-27 13:10:16 +0530

    env

--- src/lib/mongodb.js
+++ src/lib/mongodb.js
@@ -1,6 +1,6 @@
 import { MongoClient } from "mongodb";
 
-const uri = process.env.MONGO_URI; // Add this to your .env file
+const uri = await process.env.MONGO_URI; // Add this to your .env file
 const options = {};
 
 let client;
